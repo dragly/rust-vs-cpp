88cca9edb47d5e988861173a65c378969e4aefab
Svenn-Arne Dragly <s@dragly.com>
2023-06-01 22:01:28 +0200

    Add build type to run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-mkdir -p build || exit $?
 export CC=clang-15 || exit $?
 export CXX=clang++-15 || exit $?
-conan install . --output-folder=build --build=missing || exit $?
+
+mkdir -p build || exit $?
+conan install . --build=missing --output-folder=build -s build_type=$1 || exit $?
 cd build || exit $?
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=$1 \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -GNinja \
     || exit $?
 cp compile_commands.json .. || exit $?
-ninja $1 || exit $?
+ninja $2 || exit $?
 
-./$1/$1
+./$2/$2 || exit $?
