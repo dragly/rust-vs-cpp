14b21df7f9f54d3ef7f749ce8b2a9875bd90f72a
Svenn-Arne Dragly <svenn.arne.dragly@zivid.com>
2023-06-02 10:17:28 +0200

    Fix run.sh to use build folder for conan output

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ export CC=clang-15 || exit $?
 export CXX=clang++-15 || exit $?
 
 mkdir -p build || exit $?
-conan install . --build=missing --output-folder=build -s build_type=$1 || exit $?
 cd build || exit $?
+conan install .. --build=missing --output-folder=build -s build_type=$1 || exit $?
 cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake \
     -DCMAKE_BUILD_TYPE=$1 \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -GNinja \
